04d88bf40ad91299f33825c0e45dbe55d9cd5f7c
NhanThan271 <nhantran.13082005@gmail.com>
2026-01-02 18:05:14 +0700

    update

--- src/main/java/com/thantruongnhan/doanketthucmon/controller/GenreController.java
+++ src/main/java/com/thantruongnhan/doanketthucmon/controller/GenreController.java
@@ -1,6 +1,8 @@
 package com.thantruongnhan.doanketthucmon.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import com.thantruongnhan.doanketthucmon.entity.Genre;
@@ -17,26 +19,31 @@ public class GenreController {
     private final GenreService genreService;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYEE', 'CUSTOMER')")
     public List<Genre> getAllGenres() {
         return genreService.getAllGenres();
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYEE', 'CUSTOMER')")
     public Genre getGenreById(@PathVariable Long id) {
         return genreService.getGenreById(id);
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYEE')")
     public Genre createGenre(@RequestBody Genre genre) {
         return genreService.createGenre(genre);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYEE')")
     public Genre updateGenre(@PathVariable Long id, @RequestBody Genre genre) {
         return genreService.updateGenre(id, genre);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYEE')")
     public void deleteGenre(@PathVariable Long id) {
         genreService.deleteGenre(id);
     }
