18e6b212798a5e5b564379f571e037590980947f
NhanThan271 <nhantran.13082005@gmail.com>
2026-01-04 18:14:42 +0700

    update

--- src/main/java/com/thantruongnhan/doanketthucmon/service/impl/CloudinaryStorageService.java
+++ src/main/java/com/thantruongnhan/doanketthucmon/service/impl/CloudinaryStorageService.java
@@ -3,12 +3,15 @@ package com.thantruongnhan.doanketthucmon.service.impl;
 import com.cloudinary.Cloudinary;
 import com.thantruongnhan.doanketthucmon.service.StorageService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Map;
 
 @Service
+@Primary
 @RequiredArgsConstructor
 public class CloudinaryStorageService implements StorageService {
 
